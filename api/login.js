e33380b0b272710087df4169a587720545851ec4
sweat <tobi@sweatstrategies.com>
2025-07-28 17:22:39 +0100

    Update login.js

--- api/login.js
+++ api/login.js
@@ -8,7 +8,7 @@ export default function handler(req, res) {
   const scope = "user.info.basic,video.list";
   const state = "some_state"; // Für echten Einsatz sollte das randomisiert und geprüft werden.
 
-  const authUrl = `https://www.tiktok.com/auth/authorize/?client_key=${TIKTOK_CLIENT_KEY}&response_type=code&scope=${encodeURIComponent(scope)}&redirect_uri=${encodeURIComponent(TIKTOK_REDIRECT_URI)}&state=${state}`;
+  const authUrl = `https://www.tiktok.com/auth/authorize/?client_key=${TIKTOK_CLIENT_KEY}&response_type=code&scope=${encodeURIComponent(scope)}&redirect_uri=${encodeURIComponent(TIKTOK_REDIRECT_URI)}`;
 
   res.writeHead(302, { Location: authUrl });
   res.end();
