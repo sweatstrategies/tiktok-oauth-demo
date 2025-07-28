7efad8f4b80cdbf0e8fa31b07e75ecf5e5e0c9e5
sweat <tobi@sweatstrategies.com>
2025-07-28 16:42:54 +0100

    Update callback.js

--- api/callback.js
+++ api/callback.js
@@ -1,22 +1,25 @@
-// api/tiktok/callback.js
 export default async function handler(req, res) {
   const { TIKTOK_CLIENT_KEY, TIKTOK_CLIENT_SECRET, TIKTOK_REDIRECT_URI } = process.env;
 
   const code = req.query.code;
   if (!code) return res.status(400).send("Missing code");
 
-  const tokenRes = await fetch("https://open-api.tiktok.com/oauth/access_token/", {
-    method: "POST",
-    headers: { "Content-Type": "application/x-www-form-urlencoded" },
-    body: new URLSearchParams({
-      client_key: TIKTOK_CLIENT_KEY,
-      client_secret: TIKTOK_CLIENT_SECRET,
-      code,
-      grant_type: "authorization_code",
-      redirect_uri: TIKTOK_REDIRECT_URI,
-    }),
-  });
+  try {
+    const tokenRes = await fetch("https://open-api.tiktok.com/oauth/access_token/", {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: new URLSearchParams({
+        client_key: TIKTOK_CLIENT_KEY,
+        client_secret: TIKTOK_CLIENT_SECRET,
+        code,
+        grant_type: "authorization_code",
+        redirect_uri: TIKTOK_REDIRECT_URI,
+      }),
+    });
 
-  const tokenData = await tokenRes.json();
-  res.status(200).json(tokenData);
+    const tokenData = await tokenRes.json();
+    return res.status(200).json(tokenData);
+  } catch (error) {
+    return res.status(500).send("Token exchange failed");
+  }
 }
