0af71f39ad2dbbe307f80bb8ee3a191d410c450d
sweat <tobi@sweatstrategies.com>
2025-07-28 16:26:40 +0100

    Create callback.js

---
+++ api/callback.js
@@ -0,0 +1,22 @@
+// api/tiktok/callback.js
+export default async function handler(req, res) {
+  const { TIKTOK_CLIENT_KEY, TIKTOK_CLIENT_SECRET, TIKTOK_REDIRECT_URI } = process.env;
+
+  const code = req.query.code;
+  if (!code) return res.status(400).send("Missing code");
+
+  const tokenRes = await fetch("https://open-api.tiktok.com/oauth/access_token/", {
+    method: "POST",
+    headers: { "Content-Type": "application/x-www-form-urlencoded" },
+    body: new URLSearchParams({
+      client_key: TIKTOK_CLIENT_KEY,
+      client_secret: TIKTOK_CLIENT_SECRET,
+      code,
+      grant_type: "authorization_code",
+      redirect_uri: TIKTOK_REDIRECT_URI,
+    }),
+  });
+
+  const tokenData = await tokenRes.json();
+  res.status(200).json(tokenData);
+}
