798c95dae119da13c36a42408a16d5760a73e91d
sweat <tobi@sweatstrategies.com>
2025-07-28 16:43:11 +0100

    Update login.js

--- api/login.js
+++ api/login.js
@@ -2,11 +2,11 @@ export default function handler(req, res) {
   const { TIKTOK_CLIENT_KEY, TIKTOK_REDIRECT_URI } = process.env;
 
   if (!TIKTOK_CLIENT_KEY || !TIKTOK_REDIRECT_URI) {
-    return res.status(500).send("Environment variables missing");
+    return res.status(500).send("Missing environment variables");
   }
 
   const scope = "user.info.basic,video.list";
-  const state = "your_custom_state";
+  const state = "some_state";
 
   const authUrl = `https://www.tiktok.com/auth/authorize/?client_key=${TIKTOK_CLIENT_KEY}&response_type=code&scope=${encodeURIComponent(
     scope
